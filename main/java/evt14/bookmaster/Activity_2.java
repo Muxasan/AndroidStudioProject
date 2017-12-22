7580120eda34b65628b9bbd4bc4b28cb2c14742c
Muxasan <mux012@yandex.ru>
2017-12-22 23:05:17 +0500

    Add files via upload

--- main/java/evt14/bookmaster/Activity_2.java
+++ main/java/evt14/bookmaster/Activity_2.java
@@ -5,6 +5,7 @@ import android.database.SQLException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +24,7 @@ public class Activity_2 extends Activity {
         Bundle extras = getIntent().getExtras();
         String Ganrel =  extras.getString("Ganre");
         String BookName = "";
-        String BookImage = "";
+        int BookImage;
 
         DatabaseHelper myDbHelper = new DatabaseHelper(getApplicationContext());
         try {
@@ -38,23 +39,29 @@ public class Activity_2 extends Activity {
         }
         Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
 
-        RelativeLayout mainLayout = (RelativeLayout)findViewById(R.id.mainlayout);
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainlayout);
 
-        c = myDbHelper.query("Books",null,null,null,null,null,null);
+       c = myDbHelper.query("Books",null,null,null,null,null,null);
         c.moveToFirst();
         while (!c.isAfterLast()) {
             BookName = c.getString(1);
-            BookImage = c.getString(4);
+            BookImage = c.getInt(4);
 
-            int resID = getResources().getIdentifier(BookImage , "drawable", getPackageName());
+            int relativeId = 1;
+
+            RelativeLayout relativeLayout = new RelativeLayout(getApplicationContext());
+            relativeLayout.setId(relativeId);
+            mainLayout.addView(relativeLayout);
+
             ImageView imageView = new ImageView(getApplicationContext());
-            imageView.setImageResource(resID);
-            mainLayout.addView(imageView);
+            imageView.setImageResource(BookImage);
+            relativeLayout.addView(imageView);
 
             TextView textView = new TextView(getApplicationContext());
             textView.setText(BookName);
-            mainLayout.addView(textView);
+            relativeLayout.addView(textView);
 
+            relativeId = relativeId + 1;
             c.moveToNext();
         }
         c.close();
